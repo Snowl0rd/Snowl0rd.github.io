691705f290cf50e60a80f06aee78080347b72789
Snowl0rd <Dmitrytar19@gmail.com>
2023-04-02 17:28:00 +0300

    Очень тяжёлое создание переводчика

--- main.js
+++ main.js
@@ -1,14 +1,57 @@
-var alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ' ,'ъ', 'ы', 'ь', 'э', 'ю', 'я']
-<script>
-function translate() {
-if(document.getElementById('ru').value=='введите')
-document.getElementById('en').value='enter';
- 
-if(document.getElementById('ru').value=='текст')
-document.getElementById('en').value='text';
-}
-</script>
- 
-<input type="text" id="ru" />
-<input type="text" id="en" />
-<input type="button" value="Перевести" onclick="translate()" />
+(function(){
+
+    "use strict";
+    
+    window.CeasarsCipher = function(input){
+        var alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".toLowerCase(),
+              shiftAmount,
+          shiftedAlphabet = "",
+          OUTPUT = "",
+          STRING,
+          otherCharacters = '-=~\"\'#$%&*^:<>?/!{(|)}.1234567890\, '
+      if(typeof input === "object"){
+          shiftAmount = input.shift;
+        STRING = input.msg.toLowerCase();
+      } else {
+          return;
+      }
+      if(typeof STRING === "string" || typeof (STRING+"") === "string"){
+           shiftedAlphabet = alphabet.slice(shiftAmount);
+        shiftedAlphabet += alphabet.slice(0, shiftAmount);
+        shiftedAlphabet += otherCharacters;
+        alphabet += otherCharacters;
+        
+        for(var i = 0; i < STRING.length; i++){
+            var numberd = alphabet.indexOf( STRING[i] )
+            
+          OUTPUT += shiftedAlphabet[numberd];
+        }
+      } else {
+        return;
+      };
+      
+      return OUTPUT;
+      
+    };
+    })();
+    
+    
+    document.getElementById("message").addEventListener("input", function(){
+        var itsValue = this.value;
+      document.getElementById("output").textContent = CeasarsCipher({
+          msg: itsValue,
+        shift: document.getElementById("shift").value
+      });
+    });
+    document.getElementById("shift").addEventListener("keyup", function(){
+        var itsValue = this.value;
+      document.getElementById("output").textContent = CeasarsCipher({
+          msg: document.getElementById("message").value,
+        shift: itsValue
+      })
+    });
+    
+    document.getElementById("output").textContent = CeasarsCipher({
+          msg: document.getElementById("message").value,
+        shift: document.getElementById("shift").value
+      });
